598667c696188bb04e259ea6efa9812a56cb5aea
charkun704 <cvldz03@gmail.com>
2018-12-18 21:28:23 -0500

    Finishing up sort and print methods.

--- src/com/charkun704/Main.java
+++ src/com/charkun704/Main.java
@@ -6,6 +6,9 @@ public class Main {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int[] arr = getInts(5);
+        int[] sorted = sortArr(arr);
+        printArr(sorted);
     }
 
     public static int[] getInts(int arrSize){
@@ -13,5 +16,31 @@ public class Main {
         System.out.println("Enter "+arrSize+" integers.\r");
         for(int i=0;i<arrInts.length;i++){ arrInts[i] = sc.nextInt(); }
         return arrInts;
+    }
+
+    public static int[] sortArr(int[] unsorted){
+        int[] sorted = Arrays.copyOf(unsorted, unsorted.length);
+        boolean bool = true;
+        int temp = 0;
+
+        while(bool){
+            bool = false;
+
+            for(int i=0; i<sorted.length-1;i++){
+                if(sorted[i]>sorted[i+1]){
+                    temp = sorted[i];
+                    sorted[i] = sorted[i+1];
+                    sorted[i+1] = temp;
+                    bool = true;
+                }
+            }
+        }
+        return sorted;
+    }
+
+    public static void printArr(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            System.out.println("Index "+i+" has "+arr[i]+"");
+        }
     }
 }
